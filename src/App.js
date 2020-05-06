a5f4ce2229945957c1d530b0a9791217358c2787
Bharath banothu <bbanothu1997@gmail.com>
2020-05-06 14:10:11 -0700

    added caching to server side functions. Refactored code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './home';
-import Projects from './projects';
-import newProjects from './newProjects';
-import Login from './login';
-import Admin from './admin_verify';
-import Main from './main';
-import './css/index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-tabs/style/react-tabs.css';
+import Login from "./login";
+import Admin from "./admin_verify";
+import Main from "./main";
+import "./css/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-web-tabs/dist/react-web-tabs.css";
+import "react-tabs/style/react-tabs.css";
+import Async from "react-async";
+import { Tab, TabPanel, Tabs, TabList } from "react-web-tabs";
+import Loading from "./images/loading.svg";
 
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
@@ -21,7 +22,7 @@ class App extends Component {
     super(props);
     this.state = {
       menu: false,
-      menuVal: "none"
+      menuVal: "none",
     };
     this.toggleMenu = this.toggleMenu.bind(this);
   }
@@ -29,11 +30,11 @@ class App extends Component {
   // Used to toggle navbar
   toggleMenu() {
     if (this.state.menu) {
-      this.setState({ menu: !this.state.menu })
-      this.setState({ menuVal: "none" })
+      this.setState({ menu: !this.state.menu });
+      this.setState({ menuVal: "none" });
     } else {
-      this.setState({ menu: !this.state.menu })
-      this.setState({ menuVal: "inline" })
+      this.setState({ menu: !this.state.menu });
+      this.setState({ menuVal: "inline" });
     }
   }
 
@@ -41,32 +42,59 @@ class App extends Component {
   render() {
     return (
       <div id="smooth">
-            <Provider store={store}>
-        <Router>
-          <nav className="navbar navbar-expand-md navbar-light fixed-top ">
-            <button className=" pull-left navbar-toggler" type="button" onClick={this.toggleMenu} style={{ backgroundColor: "white" }}>
-              <span className=" pull-left navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse menu " style={{ display: this.state.menuVal }}>
-              {/* <div className="navbar-nav mr-auto">
+        <Provider store={store}>
+          <Router>
+            <nav className="navbar navbar-expand-md navbar-light fixed-top ">
+              <button
+                className=" pull-left navbar-toggler"
+                type="button"
+                onClick={this.toggleMenu}
+                style={{ backgroundColor: "white" }}
+              >
+                <span className=" pull-left navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse menu "
+                style={{ display: this.state.menuVal }}
+              >
+                {/* <div className="navbar-nav mr-auto">
                 <a href="/" className="nav-link" style={{ color: "white", fontSize: "1em" }}>Home</a>
                 <a href="/projects" className="nav-link" style={{ color: "white", fontSize: "1em" }}>Projects</a>
                 <a href="/newProjects" className="nav-link" style={{ color: "white", fontSize: "1em", whiteSpace: "nowrap" }}>New Projects</a>
                 <a href="/login" className="nav-link" style={{ color: "white", fontSize: "1em", whiteSpace: "nowrap" }}>Login</a>
               </div> */}
-              <div className="navbar-nav ml-auto ">
-                <a href="https://www.linkedin.com/in/bharath-banothu-54827713b/" className="nav-link" style={{ color: "white", fontSize: "1em" }}><span className="fa fa-linkedin"></span></a>
-                <a href="https://www.github.com/bbanothu" className="nav-link" style={{ color: "white", fontSize: "1em" }}><span className="fa fa-github"></span></a>
-                <a href="mailto:bbanothu1997@gmail.com" className="nav-link" style={{ color: "white", fontSize: "1em" }}><span className="fa fa-envelope-o"></span></a>
+                <div className="navbar-nav ml-auto ">
+                  <a
+                    href="https://www.linkedin.com/in/bharath-banothu-54827713b/"
+                    className="nav-link"
+                    style={{ color: "white", fontSize: "1em" }}
+                  >
+                    <span className="fa fa-linkedin"></span>
+                  </a>
+                  <a
+                    href="https://www.github.com/bbanothu"
+                    className="nav-link"
+                    style={{ color: "white", fontSize: "1em" }}
+                  >
+                    <span className="fa fa-github"></span>
+                  </a>
+                  <a
+                    href="mailto:bbanothu1997@gmail.com"
+                    className="nav-link"
+                    style={{ color: "white", fontSize: "1em" }}
+                  >
+                    <span className="fa fa-envelope-o"></span>
+                  </a>
+                </div>
               </div>
-            </div>
-          </nav>
-          <Route exact path="/" component={Main} />
-          {/* <Route exact path="/" component={Home} /> */}
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/admin" component={Admin} />
-          {/* <Route path="/projects" component={Projects} />
+            </nav>
+            <Route exact path="/" component={Main} />
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/admin" component={Admin} />
+            {/* <Route path="/projects" component={Projects} />
           <Route path="/newProjects" component={newProjects} /> */}
-        </Router>
+          </Router>
         </Provider>
       </div>
     );
--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import Async from "react-async"
+import React, { Component } from "react";
+import Async from "react-async";
 import { Tab, TabPanel, Tabs, TabList } from "react-web-tabs";
 import Loading from "./images/loading.svg";
-import "./css/index.css"
+import "./css/index.css";
 
 class main extends Component {
-  
   async getNewProjectsPage() {
     var data = [];
-    const promise1 = fetch("https://us-central1-portfolio-6b427.cloudfunctions.net/getProjects").then(res => res.json())
-    const promise2 = fetch("https://us-central1-portfolio-6b427.cloudfunctions.net/getNewProjects").then(res => res.json())
-    const promise3 = fetch("https://us-central1-portfolio-6b427.cloudfunctions.net/getNewProjectTasks").then(res => res.json())
+    const promise1 = fetch(
+      "https://us-central1-portfolio-6b427.cloudfunctions.net/getProjects"
+    ).then((res) => res.json());
+    const promise2 = fetch(
+      "https://us-central1-portfolio-6b427.cloudfunctions.net/getNewProjects"
+    ).then((res) => res.json());
+    const promise3 = fetch(
+      "https://us-central1-portfolio-6b427.cloudfunctions.net/getNewProjectTasks"
+    ).then((res) => res.json());
     data = Promise.all([promise1, promise2, promise3]);
     return data;
   }
 
-
-  // Render Function      
+  // Render Function
   render() {
     // Conditonally render images
 
-    const styles = {
-
-    }
+    const styles = {};
     const images = (image, image1, image2) => {
       if (image == null) {
-        return (
-          <div></div>
-        )
+        return <div></div>;
       } else {
         return (
           <div>
@@ -35,58 +35,102 @@ class main extends Component {
             <div className="row">
               <div id="rcorners" className="col-md-4 ">
                 <div className="thumbnail">
-                  <img className="shadow" id="rcorners" alt="" src={image} style={{ width: "100%" }} ></img>
-                  <div className="caption">
-                  </div>
+                  <img
+                    className="shadow"
+                    id="rcorners"
+                    alt=""
+                    src={image}
+                    style={{ width: "100%" }}
+                  ></img>
+                  <div className="caption"></div>
                 </div>
               </div>
               <div id="rcorners" className="col-md-4">
                 <div className="thumbnail">
-                  <img className="shadow" id="rcorners" alt="" src={image1} style={{ width: "100%" }}></img>
-                  <div className="caption">
-                  </div>
+                  <img
+                    className="shadow"
+                    id="rcorners"
+                    alt=""
+                    src={image1}
+                    style={{ width: "100%" }}
+                  ></img>
+                  <div className="caption"></div>
                 </div>
               </div>
               <div id="rcorners" className="col-md-4">
                 <div className="thumbnail">
-                  <img className="shadow" id="rcorners" alt="" src={image2} style={{ width: "100%" }}></img>
-                  <div className="caption">
-                  </div>
+                  <img
+                    className="shadow"
+                    id="rcorners"
+                    alt=""
+                    src={image2}
+                    style={{ width: "100%" }}
+                  ></img>
+                  <div className="caption"></div>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       }
-    }
+    };
 
     return (
       <Async promiseFn={this.getNewProjectsPage}>
         {({ data, error, isLoading }) => {
-          if (isLoading) return <div style={{ marginTop: "3em" }}>
-            <img alt="loading" src={Loading} style={{
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto"
-            }} ></img></div>
-          if (error) return `Something went wrong: ${error.message}`
+          if (isLoading)
+            return (
+              <div style={{ marginTop: "3em" }}>
+                <img
+                  alt="loading"
+                  src={Loading}
+                  style={{
+                    display: "block",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                  }}
+                ></img>
+              </div>
+            );
+          if (error) return `Something went wrong: ${error.message}`;
           if (data)
             return (
-              <Tabs defaultTab={data[0].title} vertical style={{ color: "white", marginTop: "3em" }}>
-                <TabList className="shadow p-3 mb-5 bg-white rounded mt-4 ml-3 " style={{ height: "80%" }}>
+              <Tabs
+                defaultTab={data[0].title}
+                vertical
+                style={{ color: "white", marginTop: "3em" }}
+              >
+                <TabList
+                  className="shadow p-3 mb-5 bg-white rounded mt-4 ml-3 "
+                  style={{ height: "80%" }}
+                >
                   {data[0].map((project, index) => (
-                    <Tab key={index} tabFor={project.title} >{project.title}</Tab>
+                    <Tab key={index} tabFor={project.title}>
+                      {project.title}
+                    </Tab>
                   ))}
 
                   {data[1].map((project, index) => (
-                    <Tab key={index} tabFor={project.title} >{project.title}</Tab>
+                    <Tab key={index} tabFor={project.title}>
+                      {project.title}
+                    </Tab>
                   ))}
-
                 </TabList>
                 {data[0].map((project, index) => (
-                  <TabPanel key={index} tabId={project.title} style={{ color: "black", width: "80%" }}>
+                  <TabPanel
+                    key={index}
+                    tabId={project.title}
+                    style={{ color: "black", width: "80%" }}
+                  >
                     <div className="shadow p-3 mb-5 bg-white rounded mt-4 ml-3 ">
-                      <h1><a style={{ textDecoration: "none", color: "black" }} href={project.github}>{project.title}</a></h1>
+                      <h1>
+                        <a
+                          style={{ textDecoration: "none", color: "black" }}
+                          href={project.github}
+                        >
+                          {project.title}
+                        </a>
+                      </h1>
                       <p>Project Description: {project.description}</p>
                       <p>Technologies Used: {project.tech}</p>
                       {images(project.image, project.image1, project.image2)}
@@ -95,10 +139,21 @@ class main extends Component {
                     </div>
                   </TabPanel>
                 ))}
-                  {data[1].map((project, index) => (
-                  <TabPanel key={index} tabId={project.title} style={{ color: "black", width: "80%" }}>
+                {data[1].map((project, index) => (
+                  <TabPanel
+                    key={index}
+                    tabId={project.title}
+                    style={{ color: "black", width: "80%" }}
+                  >
                     <div className="shadow p-3 mb-5 bg-white rounded mt-4  ">
-                      <h1><a style={{ textDecoration: "none", color: "black" }} href={project.github}>{project.title}</a></h1>
+                      <h1>
+                        <a
+                          style={{ textDecoration: "none", color: "black" }}
+                          href={project.github}
+                        >
+                          {project.title}
+                        </a>
+                      </h1>
                       <p>My Role: {project.role}</p>
                       <p>Project Description: {project.description}</p>
                       <p>Technologies Used: {project.tech}</p>
@@ -116,14 +171,18 @@ class main extends Component {
                             </tr>
                           </thead>
                           <tbody>
-                            {data[2].filter(project1 => project1.id.includes(project.title)).map((project1, index) => (
-                              <tr key={index} index={index}>
-                                <th>{project1.task}</th>
-                                <td>{project1.startdate}</td>
-                                <td>{project1.status}</td>
-                                <td>{project1.enddate}</td>
-                              </tr>
-                            ))}
+                            {data[2]
+                              .filter((project1) =>
+                                project1.id.includes(project.title)
+                              )
+                              .map((project1, index) => (
+                                <tr key={index} index={index}>
+                                  <th>{project1.task}</th>
+                                  <td>{project1.startdate}</td>
+                                  <td>{project1.status}</td>
+                                  <td>{project1.enddate}</td>
+                                </tr>
+                              ))}
                           </tbody>
                         </table>
                       </div>
@@ -131,8 +190,8 @@ class main extends Component {
                   </TabPanel>
                 ))}
               </Tabs>
-            )
-          return null
+            );
+          return null;
         }}
       </Async>
     );
